3e77215a6125882f1f8df3d72623c8ca655d3b7a
Lmnhutw <lmnhut0211@gmail.com>
2024-06-17 00:05:05 +0700

    Add Login page

---
+++ src/components/AuthForm.tsx
@@ -0,0 +1,43 @@
+import styles from './AuthForm.module.css';
+import Link from 'next/link';
+
+type AuthFormProps = {
+    type: 'login' | 'register';
+};
+
+const AuthForm: React.FC<AuthFormProps> = ({ type }) => {
+    return (
+        <div className={styles.container}>
+            <h1 className={styles.title}>Welcome to ChatApp</h1>
+            <form className={styles.form}>
+                <label className={styles.label}>
+                    Email
+                    <input type="email" className={styles.input} placeholder="you@example.com" />
+                </label>
+                <label className={styles.label}>
+                    Password
+                    <input type="password" className={styles.input} placeholder="••••••••" />
+                </label>
+                <button type="submit" className={styles.button}>
+                    {type === 'login' ? 'Login' : 'Sign Up'}
+                </button>
+            </form>
+            {type === 'login' ? (
+                <div className={styles.footer}>
+                    <p>
+                        
+                        You don&apos;t have an account? <Link href="/register">Sign Up</Link>
+                    </p>
+                </div>
+            ) : (
+                <div className={styles.footer}>
+                    <p>
+                        Already have an account? <Link href="/login">Login</Link>
+                    </p>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default AuthForm;
